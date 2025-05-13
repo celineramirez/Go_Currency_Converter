e42435660db1e76999ce1aa81a5d9973db73cb71
celineramirez <crmrz28@gmail.com>
2025-05-12 21:11:03 -0400

    Setting up currency converter api key

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+import (
+	"fmt"
+	// "net/http"
+	// "github.com/charmbracelet/huh"
+	// "encoding/json"
+	"os"
+)
+
+func main() {
+	apiKey := os.Getenv("CURRENCY_CONV_API_KEY")
+	
+}
