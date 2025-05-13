d76f36635c3d21b36ae269ef5f1a19b9a73f2e92
celineramirez <crmrz28@gmail.com>
2025-05-12 21:54:02 -0400

    Adding TUI

--- main.go
+++ main.go
@@ -2,20 +2,63 @@ package main
 import (
 	"fmt"
 	// "net/http"
-	// "github.com/charmbracelet/huh"
+	"github.com/charmbracelet/huh"
 	// "encoding/json"
 	"os"
 )
 
 var (
 	base	 	string
-	convert		string
+	convertTo	string
 	rateFrom	int
 	rateTo		int
 	converted	int
 )
 
 func main() {
-	apiKey := os.Getenv("CURRENCY_CONV_API_KEY")
+	// apiKey := os.Getenv("CURRENCY_CONV_API_KEY")
 	
+	form := huh.NewForm(
+		huh.NewGroup(
+        huh.NewSelect[string]().
+		Title("What is your base currency?").
+		Options(
+                huh.NewOption("USD", "usd"),
+                huh.NewOption("GBP", "gbp"),
+                huh.NewOption("EUR", "eur"),
+                huh.NewOption("JPY", "jpy"),
+            ).
+		Value(&base),
+		),
+	)
+
+	huh.NewGroup(
+	huh.NewSelect[string]().
+	Title("What currency do you want to convert to?").
+	Options(
+			huh.NewOption("USD", "usd"),
+			huh.NewOption("GBP", "gbp"),
+			huh.NewOption("EUR", "eur"),
+			huh.NewOption("JPY", "jpy"),
+		).
+	Value(&convertTo),
+
+	huh.NewInput().
+		Title("How much to convert?").
+		Value(&rateFrom),
+		Validate(func(amt int) error{
+			if amt <= 0 {
+                return fmt.Errorf("Please enter a value greater than 0")
+            }
+            return nil
+		}),
+	)
+
+	// Run the Terminal User Interface (TUI)
+	err := form.Run()
+	if err != nil {
+		fmt.Println("Unable to run currency converter")
+		os.Exit(1)
+	}
+
 }
