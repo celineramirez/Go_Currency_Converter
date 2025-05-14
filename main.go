1fba84224bcf01bb3d2819a8315bfb65faa640b2
celineramirez <crmrz28@gmail.com>
2025-05-13 22:46:30 -0400

    go fmt

--- main.go
+++ main.go
@@ -10,62 +10,60 @@ import (
 )
 
 var (
-	base	 	string
-	convertTo	string
-	rateFrom	int
-	rateTo		int
-	converted	int
+	base      string
+	convertTo string
+	rateFrom  int
+	rateTo    int
+	converted int
 )
 
 func main() {
 	// apiKey := os.Getenv("CURRENCY_CONV_API_KEY")
-	
+
 	form := huh.NewForm(
 		huh.NewGroup(
-        huh.NewSelect[string]().
-		Title("What is your base currency?").
-		Options(
-                huh.NewOption("USD", "usd"),
-                huh.NewOption("GBP", "gbp"),
-                huh.NewOption("EUR", "eur"),
-                huh.NewOption("JPY", "jpy"),
-            ).	
-		Value(&base),
+			huh.NewSelect[string]().
+				Title("What is your base currency?").
+				Options(
+					huh.NewOption("USD", "usd"),
+					huh.NewOption("GBP", "gbp"),
+					huh.NewOption("EUR", "eur"),
+					huh.NewOption("JPY", "jpy"),
+				).
+				Value(&base),
 		),
 
-	huh.NewGroup(
-	huh.NewSelect[string]().
-	Title("What currency do you want to convert to?").
-	Options(
-			huh.NewOption("USD", "usd"),
-			huh.NewOption("GBP", "gbp"),
-			huh.NewOption("EUR", "eur"),
-			huh.NewOption("JPY", "jpy"),
-		).
-	Validate(func(x string) error {
-			if x == base {
-				return errors.New("Cannot choose the same currency you are converting from")
-			}
+		huh.NewGroup(
+			huh.NewSelect[string]().
+				Title("What currency do you want to convert to?").
+				Options(
+					huh.NewOption("USD", "usd"),
+					huh.NewOption("GBP", "gbp"),
+					huh.NewOption("EUR", "eur"),
+					huh.NewOption("JPY", "jpy"),
+				).
+				Validate(func(x string) error {
+					if x == base {
+						return errors.New("Cannot choose the same currency you are converting from")
+					}
 
-			return nil
-		}).
-	Value(&convertTo),
-	),
+					return nil
+				}).
+				Value(&convertTo),
+		),
 
-	huh.NewGroup(
-	huh.NewInput().
-		Title("How much to convert?").
-		Value(&rateFrom),
-		Validate(func(x string) error{
-			if _, err := strconv.ParseFloat(x, 64); err != nil {
-                return fmt.Errorf("Please enter a valid value of currency")
-            }
-            return nil
-		}),
-	),
-
-)
-
+		huh.NewGroup(
+			huh.NewInput().
+				Title("How much to convert?").
+				Validate(func(x string) error {
+					if _, err := strconv.ParseFloat(x, 64); err != nil {
+						return fmt.Errorf("Please enter a valid value of currency")
+					}
+					return nil
+				}),
+			Value(&rateFrom),
+		),
+	) // end of form
 
 	// Run the Terminal User Interface (TUI)
 	err := form.Run()
